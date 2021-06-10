e8e2e73c3c3ecf54dad9349e17b2bd2950e5332d
rudolphcordell <84486798+rudolphcordell@users.noreply.github.com>
2021-06-09 21:22:52 -0700

    initial commit

---
+++ DSI_PremPrep.py
@@ -0,0 +1,19 @@
+# python overview
+# data types
+# ints, floats
+# bool
+# None
+# str
+
+# list
+# discribe mutability vs immutability
+
+# abstractions
+# control Flow
+# interation
+# for loops are less dangerous. More elegant. Few less lines of code. Easier to read.
+# while loopes are more powerful than for loops. need breaking condition if not it can run forever.
+#  txt = "hello"
+# print(txt)
+# txt += " World"
+# print(txt)
---
+++ Development Environment.py
@@ -0,0 +1,16 @@
+# import sys
+
+
+# print(sys.version)
+# # print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = "hello, {}".format(who_to_greet)
+#     return greeting
+
+
+# print(greet("World"))
+# print(greet("Cordell"))
+# print("nothing")
+name = input("Your Name? ")
